d0bbf44afaeec0d36df885949d28ad2ede3773dd
Martin Menabue <martinmnb@live.it>
2023-08-16 17:51:26 +0200

    Update

--- README.md
+++ README.md
@@ -4,22 +4,25 @@ Continuously sync offline wandb runs.
 ## Why?
 If you work on computing nodes without internet access, you can use wandb in offline mode to log your runs. Normally you would sync an offline run to wandb at the end of the run, but if you have a long running job, you may want to sync your run during job's execution.
 
-## Get Started
+## How it works
 This project has two components:
 * Farm: a https server that listens for sync requests and syncs the runs to wandb.
-* Agent: a python module that you use in your code to request a sync to the farm.
+* Agent: a python module that you use in your code to request the syncs to the farm.
 
-To get started:
-* Install the package: `pip install wandb-offline-sync`
-* Generate a SSL certificate for the sync farm. You can use the command: `openssl req -newkey rsa:4096 -nodes -keyout key.pem -x509 -days 365 -out cert.pem`
+## Quickstart
+* Install this package: `pip install wandb-offline-sync`
+* Generate a SSL certificate for the sync farm. You can use the command: `openssl req -newkey rsa:4096 -nodes -keyout key.pem -x509 -days 365 -out cert.pem`. This command will create two files: `cert.pem` and `key.pem`.
 * Run `export WANDB_SYNC_FARM_USERNAME=<your_username>; export WANDB_SYNC_FARM_PASSWORD=<your_password>` to set the username and password for the sync farm. You can also put these commands in your `.bashrc` file. Replace `<your_username>` and `<your_password>` with your credentials. If these variables are not set, the farm will use the default credentials `("user", "pass")`
-* Run the farm with the command `wandb_sync_farm` in a node with internet connection. After that, the farm will listen for sync requests. Run `wandb_sync_farm --help` to see all the available options.
+* Run the farm with the command `wandb_sync_farm --cert=<path_to_cert.pem> --key=<path_to_key.pem>` in a node with internet connection. The farm will listen for sync requests. Run `wandb_sync_farm --help` to see all the available options.
 * Run `export WANDB_SYNC_FARM_HOST=<sync_farm_ip_address_or_hostname>; export WANDB_SYNC_FARM_PORT=<sync_farm_port>` to set the hostname and port of the sync farm. You can also put these commands in your `.bashrc` file. These variables will be used by the agent.
 
-Then, in the code of your job, to use the agent:
-* import the agent: `from wandb_offline_sync import agent`
-* After calling `wandb.init(...)`, initialize the agent with: `agent.init()`. You can pass a `frequency` argument to set the minimum time between syncs (in seconds). For example, if you set `frequency=60`, the agent will request a sync at most once per minute. The default value for the frequency is 5 minutes.
-* After each call to `wandb.log(...)`, call `agent.trigger_sync()` to request a sync to the farm.
+In the code of your job:
+* Import the agent: `from wandb_offline_sync import agent`
+* After calling `wandb.init(...)`, initialize the agent with: `agent.init(...)`. You can pass a `frequency` argument to set the minimum time between syncs (in seconds). For example, if you set `frequency=60`, the agent will request a sync at most once per minute. The default value for the frequency is 5 minutes.
+
+Each time `wandb.log(...)` is called, the agent will check if the minimum time interval (given by the frequency) between syncs has passed, and if so, it will request a sync to the farm.
+
+When `wandb.finish()` is called, the agent will wait some seconds (default is 30) to ensure that previous syncs have been completed, and then it will request a final sync to the farm.
 
 ## Notes
-* In the first minutes of the run, it may happen that the sync farm fails in syncing the run to wandb. Don't worry, after some minutes the sync farm should be able to sync the run. You can always set the --verbose option when you run the sync farm to see what is happening.
+* If you run the farm with `--verbose`, it may happen that in the first minutes of the run, the output shows the error `.wandb file is empty`, and the run is not synced to the wandb server. Don't worry, after some minutes the data of the run will be available and will be synced to the wandb server.
--- wandb_offline_sync/agent/wandb_sync_agent.py
+++ wandb_offline_sync/agent/wandb_sync_agent.py
@@ -3,6 +3,7 @@ import os
 import wandb
 import time
 import sys
+from functools import wraps
 
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
@@ -17,42 +18,80 @@ class SyncAgent:
             cls.instance = super().__new__(cls)
             return cls.instance
     
-    def init(self, frequency=300, timeout=5, verbose=False):
+    def decorate_wandb(self):
+        def wrapper_log(func):
+            @wraps(func)
+            def wrapped(*args, **kwargs):
+                result = func(*args, **kwargs)
+                self.trigger_sync()
+                return result
+            return wrapped
+        wandb.log = wrapper_log(wandb.log)
+
+        def wrapper_finish(func):
+            @wraps(func)
+            def wrapped(*args, **kwargs):
+                result = func(*args, **kwargs)
+                self.trigger_sync(force=True)
+                return result
+            return wrapped
+        wandb.finish = wrapper_finish(wandb.finish)
+    
+    def init(self, frequency=300, timeout=5, force_wait_time=30, decorate=True, verbose=False):
         self.frequency = frequency
         self.timeout = timeout
         self.verbose = verbose
+        self.force_wait_time = force_wait_time
+        self.decorate = decorate
 
         try:
             self.run_id = wandb.run.id
             self.run_dir = wandb.run.dir.removesuffix('/files')
-            self.data = {
-                'run_id': self.run_id,
-                'run_dir': self.run_dir.removesuffix('/files'),
-            }
-            self.old_time = time.time()
         except:
             print('SYNC FARM AGENT - ERROR: wandb is not initialized. Call wandb.init() before initializing the agent', file=sys.stderr)
+            return
+        
+        self.data = {
+                'run_id': self.run_id,
+                'run_dir': self.run_dir.removesuffix('/files'),
+        }
+        self.old_time = time.time()
+        if self.decorate:
+            self.decorate_wandb()
     
     def trigger_sync(self, force=False):
         if not hasattr(self, 'run_id'):
             print('WANDB SYNC FARM AGENT - ERROR: agent is not initialized. Call agent.init() before triggering sync', file=sys.stderr)
             return
+    
+        if force:
+            if self.verbose:
+                print(f'WANDB SYNC FARM AGENT - Forcing sync. Waiting for {self.force_wait_time} seconds before requesting a sync, ' + \
+                      'to ensure that previous syncs have been completed')
+            time.sleep(self.force_wait_time)
+            self.old_time = time.time()
+            self._request_sync()
+            return
         
         if not force and (time.time() - self.old_time) < self.frequency:
             if self.verbose:
-                print('WANDB SYNC FARM AGENT - Delay has not passed. Not triggering sync')
+                print('WANDB SYNC FARM AGENT - The time interval between syncs has not passed. Not triggering sync')
             return
         
         self.old_time = time.time()
         try:
-            if self.verbose:
-                print('WANDB SYNC FARM AGENT - Sending sync request')
-            r = requests.post(f'https://{HOSTNAME}:{PORT}/sync', verify=False,
-                                auth=(WANDB_SYNC_FARM_USERNAME, WANDB_SYNC_FARM_PASSWORD),
-                                data=self.data, timeout=self.timeout)
-            return r
+            self._request_sync()
+        
         except Exception as e:
             if self.verbose:
-                print('WANDB SYNC FARM AGENT - ERROR: sync farm not responding', file=sys.stderr)
+                print(f'WANDB SYNC FARM AGENT - ERROR: sync farm (https://{HOSTNAME}:{PORT}) not responding', file=sys.stderr)
                 print(e)
+        
+    def _request_sync(self):
+        if self.verbose:
+            print('WANDB SYNC FARM AGENT - Sending sync request')
+        r = requests.post(f'https://{HOSTNAME}:{PORT}/sync', verify=False,
+                auth=(WANDB_SYNC_FARM_USERNAME, WANDB_SYNC_FARM_PASSWORD),
+                data=self.data, timeout=self.timeout)
+        return r
         
